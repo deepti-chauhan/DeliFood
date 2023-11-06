e27dfdde78441b2a478e15579302bd8bedceb123
Deepti <deepti22599@gmail.com>
2023-11-06 23:36:01 +0530

    feat : Add User Profile Component
     to show current user's name, email, Ph.no & Basic details

--- frontend/src/components/profile/UserProfile.js
+++ frontend/src/components/profile/UserProfile.js
@@ -1,20 +1,34 @@
+import './UserProfile.css'
 const UserProfile = () => {
   const { username, email } = JSON.parse(localStorage.getItem('user'))
   return (
-    <div>
-      <form>
-        <label for='username'>Name</label>
-        <input
-          type='text'
-          name='username'
-          id='username'
-          value={username}
-          disabled
-        />
-        <br/>
-        <label for='email'>Email</label>
-        <input type='text' name='email' id='email' value={email} disabled />
-      </form>
+    <div className='flex-center'>
+      <div className='profile-wrapper flex-center'>
+        <form className='profile-form'>
+          <div className='flex-sb'>
+            <label for='username'>Name</label>
+            <input
+              type='text'
+              name='username'
+              id='username'
+              value={username}
+              disabled
+            />
+          </div>
+          <div className='flex-sb'>
+            <label for='email'>Email</label>
+            <input type='text' name='email' id='email' value={email} disabled />
+          </div>
+          <div className='flex-sb'>
+            <label for='phone'>Phone </label>
+            <input type='text' name='phone' id='phone'/>
+          </div>
+          <div className='flex-sb'>
+            <label for='address'>Address</label>
+            <textarea type='text' name='address' id='address'/>
+          </div>
+        </form>
+      </div>
     </div>
   )
 }
