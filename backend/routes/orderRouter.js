1bd50c212c0322f15f29a0da4104a6bbc1f72a38
Deepti <deepti22599@gmail.com>
2023-11-07 13:17:13 +0530

    feat(backend) : add route for order history api

--- backend/routes/orderRouter.js
+++ backend/routes/orderRouter.js
@@ -1,8 +1,10 @@
 const express = require('express')
 const router = express.Router()
 
-const { createOrder } = require('../controllers/orderController')
+const { createOrder, getOrders, getAddress } = require('../controllers/orderController')
 
-router.post('/', createOrder)
+router.post('/orders', createOrder)
+router.post('/orderhistory', getOrders)
+router.post('/address', getAddress)
 
 module.exports = router
