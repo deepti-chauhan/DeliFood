2873560c332cfd175a75571224b65b22dd8d75d1
Deepti <deepti22599@gmail.com>
2023-11-16 03:12:52 +0530

    feat(backend) : add uuid package
    add uuid for each new address entry
    
    getAllAddress controller to fetch all existing address
    
    setAddress controller to add new address

--- backend/controllers/orderController.js
+++ backend/controllers/orderController.js
@@ -1,4 +1,6 @@
 const { default: mongoose } = require('mongoose')
+const UserModel = require('../models/user')
+const uuid = require('uuid')
 
 const orderSchema = new mongoose.Schema({
   user: {},
@@ -24,7 +26,7 @@ const getOrders = async (req, res) => {
     const { email } = req.body
     const orders = await Order.find(
       { 'user.email': email },
-      { _id: 0, 'ordereditems': true, 'user.date':true }
+      { _id: 0, ordereditems: true, 'user.date': true }
     )
     return res.status(200).send(orders)
   } catch (error) {
@@ -44,4 +46,59 @@ const getAddress = async (req, res) => {
   }
 }
 
-module.exports = { createOrder, getOrders, getAddress }
+const getAllAddress = async (req, res) => {
+  try {
+    const { email } = req.body
+    const allAddresses = await UserModel.find(
+      { email: email },
+      { _id: 0, address: true }
+    )
+
+    return res.status(200).send(allAddresses[0].address)
+  } catch (error) {
+    return res.status(400).send({ message: 'not able to fetch address' })
+  }
+}
+
+const setAddress = async (req, res) => {
+  try {
+    const { email, address } = req.body
+    const newAddress = { ...address, uuid: uuid.v4() }
+    // const uuid = uuid()
+
+    /*
+     */
+
+    const filter = { email: email }
+    console.log(address)
+    console.log(email)
+    const update = {
+      // $set : { 'address.uuid' : uuid.v4()},
+      $push: { address: newAddress },
+
+      // $setOnInsert:  address: address ,
+    }
+    options = {
+      upsert : true
+    }
+
+    // const updatedUser = await UserModel.findOneAndUpdate(filter, update)
+    console.log(UserModel)
+    const updatedUser = await UserModel.updateOne(filter, update, options)
+
+    console.log(`address: ${JSON.stringify(req.body)}`)
+    console.log(`update user address: ${JSON.stringify(updatedUser)}`)
+    return res.status(201).json({ message: 'address updated!!' })
+  } catch (error) {
+    console.log(error)
+    return res.status(400).json({ message: 'address cannot be updated!!' })
+  }
+}
+
+module.exports = {
+  createOrder,
+  getOrders,
+  getAddress,
+  setAddress,
+  getAllAddress,
+}
