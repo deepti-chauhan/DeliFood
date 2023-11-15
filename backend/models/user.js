02623151166f7d54d57051c98f648c79985701a1
Deepti <deepti22599@gmail.com>
2023-11-16 03:08:23 +0530

    fix(backend) : add AddressSchema to Usermodel

--- backend/models/user.js
+++ backend/models/user.js
@@ -1,9 +1,39 @@
+// import { model } from 'mongoose'
+
 const mongoose = require('mongoose')
 
+const addressSchema = new mongoose.Schema({
+  uuid: {
+    type: String,
+    unique: true,
+  },
+  addressType: {
+    type: String,
+  },
+  addressLocation: {
+    type: String,
+    // required: true,
+  },
+  city: {
+    type: String,
+    // required: true,
+  },
+  state: {
+    type: String,
+    // required: true,
+  },
+  postalCode: {
+    type: String,
+    // required: true,
+  },
+})
+
 const UserModel = mongoose.model('users', {
-  username: String,
-  email: String,
-  password: String,
+  username: { type: String, required: true },
+  email: { type: String, required: true },
+  password: { type: String, required: true },
+  address: [addressSchema],
 })
 
 module.exports = UserModel
+``
