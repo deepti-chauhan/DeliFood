fc6466c1ef0788471e0ec80a5ce95804d4166523
Deepti <deepti22599@gmail.com>
2023-11-16 03:04:07 +0530

    feat(backend) : add routes for newAddress & getAlladdress api

--- backend/routes/orderRouter.js
+++ backend/routes/orderRouter.js
@@ -1,10 +1,18 @@
 const express = require('express')
 const router = express.Router()
 
-const { createOrder, getOrders, getAddress } = require('../controllers/orderController')
+const {
+  createOrder,
+  getOrders,
+  getAddress,
+  setAddress,
+  getAllAddress,
+} = require('../controllers/orderController')
 
 router.post('/orders', createOrder)
 router.post('/orderhistory', getOrders)
 router.post('/address', getAddress)
+router.post('/newaddress', setAddress)
+router.post('/alladdress', getAllAddress)
 
 module.exports = router
