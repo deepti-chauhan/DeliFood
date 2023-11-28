c0ab57d9af6cab14b21ee71021f3f86e1cd5caa6
Deepti <deepti22599@gmail.com>
2023-11-28 18:41:55 +0530

    style : Add style

--- frontend/src/components/address/Payment.js
+++ frontend/src/components/address/Payment.js
@@ -3,7 +3,7 @@ import { faLocationDot } from '@fortawesome/free-solid-svg-icons'
 import { Link } from 'react-router-dom'
 import { useGlobalCartContext } from '../../store/CartProvider'
 import './style/payment.css'
-import {loadStripe} from '@stripe/stripe-js';
+import { loadStripe } from '@stripe/stripe-js'
 import env from 'react-dotenv'
 
 const Payment = ({ selectedAddress, setPayments }) => {
@@ -17,45 +17,49 @@ const Payment = ({ selectedAddress, setPayments }) => {
   }
 
   //payment integration
-  const paymentHandler = async() => {
-
-    const stripe = await loadStripe("pk_test_51OH1UASAYSPowgwnRwYxrzV1HHrnOeNp8GbRKXDHVFj6BgM5VPoSS1kOJcUKfnCdhI3zaN8QsTHZdx7QMLbQriQA00loWo6ZLZ") 
+  const paymentHandler = async () => {
+    const stripe = await loadStripe(
+      'pk_test_51OH1UASAYSPowgwnRwYxrzV1HHrnOeNp8GbRKXDHVFj6BgM5VPoSS1kOJcUKfnCdhI3zaN8QsTHZdx7QMLbQriQA00loWo6ZLZ'
+    )
 
     const body = {
-      products : cartCtx.items,
-      total : cartTotal
+      products: cartCtx.items,
+      total: cartTotal,
     }
 
     const headers = {
-      'Content-type' : 'application/json',
-      'Authorization' : token
+      'Content-type': 'application/json',
+      Authorization: token,
     }
 
-    const response = await fetch(`${env.BASE_URL}/api/create-checkout-session`,{
-      method : 'POST',
-      body   : JSON.stringify(body),
-      headers : headers
-    })
+    const response = await fetch(
+      `${env.BASE_URL}/api/create-checkout-session`,
+      {
+        method: 'POST',
+        body: JSON.stringify(body),
+        headers: headers,
+      }
+    )
 
     const session = await response.json()
 
     const result = stripe.redirectToCheckout({
-      sessionId:session.id 
+      sessionId: session.id,
     })
 
-    if(result.error){
+    if (result.error) {
       console.log(result.error)
     }
   }
-  
 
   return (
     <div>
-      <div className='flex-sb'>
-        <div>
+      <div className='payment-box flex-sb'>
+        <div className=''>
           <p>
             Deliver at <FontAwesomeIcon icon={faLocationDot} />
           </p>
+          <p className='hl'></p>
           <p>{`${selectedAddress.addressLocation}`}</p>
           <p>
             {` 
@@ -72,7 +76,9 @@ const Payment = ({ selectedAddress, setPayments }) => {
       </div>
       <div className='payment-btn-container flex-center'>
         <Link to='/payments'>
-          <button className='btn payment-btn' onClick={paymentHandler}>PAY ${cartTotal}</button>
+          <button className='btn payment-btn' onClick={paymentHandler}>
+            PAY ${cartTotal}
+          </button>
         </Link>
       </div>
     </div>
