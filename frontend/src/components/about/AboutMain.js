5b2b7f91672563bacacbb078ab199fe0a8cb33b5
Deepti <deepti22599@gmail.com>
2023-11-30 19:35:28 +0530

    fix : styling and arrangement of components

--- frontend/src/components/about/AboutMain.js
+++ frontend/src/components/about/AboutMain.js
@@ -1,55 +1,17 @@
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import {
   faBurger,
-  faHeart,
   faPizzaSlice,
   faSeedling,
 } from '@fortawesome/free-solid-svg-icons'
 import './AboutMain.css'
+import env from 'react-dotenv'
 const AboutMain = () => {
   return (
     <>
-      <div className='about-container'>
-        <div className='about-content flex-center'>
-          <h2>OUR STORY</h2>
-          <p>
-            <i>
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc
-              vehicula risus ac lacus sagittis, eget viverra justo euismod.
-              Fusce eu neque eget eros tincidunt interdum. Fusce justo massa,
-              pulvinar sed molestie sed, semper iaculis sapien. Curabitur vitae
-              velit auctor, viverra risus id, maximus magna. Nunc id ultricies
-              quam. Interdum et malesuada fames ac ante ipsum primis in
-              faucibus. In ultrices commodo pellentesque. Duis pulvinar, mi ac
-              iaculis venenatis, enim tellus ultrices nisl, id auctor lectus
-              justo et ipsum.
-            </i>
-          </p>
-        </div>
-        <div className='about-content-img ' id='img1'></div>
-        <div className='about-content-img' id='img2'></div>
-        <div className='about-content flex-center'>
-          <h2>OUR STORY</h2>
-          <p>
-            <i>
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc
-              vehicula risus ac lacus sagittis, eget viverra justo euismod.
-              Fusce eu neque eget eros tincidunt interdum. Fusce justo massa,
-              pulvinar sed molestie sed, semper iaculis sapien. Curabitur vitae
-              velit auctor, viverra risus id, maximus magna. Nunc id ultricies
-              quam. Interdum et malesuada fames ac ante ipsum primis in
-              faucibus. In ultrices commodo pellentesque. Duis pulvinar, mi ac
-              iaculis venenatis, enim tellus ultrices nisl, id auctor lectus
-              justo et ipsum.
-            </i>
-          </p>
-        </div>
-      </div>
-
-      {/* <div className='about-content-2'></div> */}
       <div className='about-content-4 flex-center'>
         <div>
-          <img src='./assets/about_assets/img-1.webp' width='400' />
+          <img src={`${env.BASE_URL}/img/img-1.webp`} width='400' />
           <span className='flex-center'>
             <FontAwesomeIcon icon={faBurger} className='about-icon' />
 
@@ -65,7 +27,7 @@ const AboutMain = () => {
               justo et ipsum.
             </p>
           </span>
-          <img src='./assets/about_assets/img-7.webp' width='400' />
+          <img src={`${env.BASE_URL}/img/img-7.webp`} width='400' />
           <span className='flex-center'>
             <FontAwesomeIcon icon={faSeedling} className='about-icon' />
 
@@ -81,7 +43,7 @@ const AboutMain = () => {
               justo et ipsum.
             </p>
           </span>
-          <img src='./assets/about_assets/img-8.webp' width='400' />
+          <img src={`${env.BASE_URL}/img/img-8.webp`} width='400' />
           <span className='flex-center'>
             <FontAwesomeIcon icon={faPizzaSlice} className='about-icon' />
             <p>
