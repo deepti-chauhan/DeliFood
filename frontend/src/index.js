13bac9da4d798c61fd6006bfa176b318a288db60
Deepti <deepti22599@gmail.com>
2023-11-30 19:58:10 +0530

    docs : add npm dependencies

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,5 +1,10 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+
+// import 'mdb-react-ui-kit/dist/css/mdb.min.css';
+// import "@fortawesome/fontawesome-free/css/all.min.css";
+
+
 import './index.css'
 // import './styles.css'
 import App from './App'
@@ -11,11 +16,10 @@ import { store } from './redux/store'
 const root = ReactDOM.createRoot(document.getElementById('root'))
 root.render(
   <React.StrictMode>
-    {/* <CartProvider>
-    </CartProvider> */}
-    <Provider store={store}>
+    <CartProvider>
       <App />
     </CartProvider>
+    {/* <Provider store={store} /> */}
   </React.StrictMode>
 )
 reportWebVitals()
