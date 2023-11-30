9793269dfc709432eefefe57fcf1877edcf08f18
Deepti <deepti22599@gmail.com>
2023-11-30 19:47:19 +0530

    feat : add static components

---
+++ frontend/src/components/shared/Popular.js
@@ -0,0 +1,97 @@
+import React, { useEffect, useState } from 'react'
+import Card from './Card'
+import '../styles/Popular.css'
+import env from 'react-dotenv'
+import CardSkeleton from './CardSkeleton'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faUtensils, faStar } from '@fortawesome/free-solid-svg-icons'
+import { Link } from 'react-router-dom'
+
+const Popular = () => {
+  const [query, setQuery] = useState('salad')
+  const [data, setData] = useState([])
+  const [loader, setLoader] = useState(true)
+
+  const getFoodCategory = data
+    .map((foodCategory) => foodCategory.category)
+    .reduce((accumulator, foodCategory) => {
+      if (!accumulator.includes(foodCategory)) {
+        accumulator.push(foodCategory)
+      }
+      return accumulator
+    }, [])
+
+  const fetchInfo = async () => {
+    try {
+      return await fetch(`${env.BASE_URL}/api/dishes/popular`, {
+        method: 'GET',
+      })
+        .then((res) => res.json())
+        .then((d) => setData(Object.values(d)))
+    } catch (error) {
+      console.log(error)
+    } finally {
+      setLoader(false)
+    }
+  }
+
+  useEffect(() => {
+    fetchInfo()
+  }, [])
+
+  return (
+    <div className='container flex-center'>
+      <div className='popular-container flex-center'>
+        <p>
+          <FontAwesomeIcon icon={faUtensils} />
+        </p>
+        <h2 className='hdr-text'>
+          Popular <span>{query}s</span> on Menu{' '}
+        </h2>
+        <div className='popular-btn flex-center'>
+          {getFoodCategory.map((foodCategory) => (
+            <button onClick={() => setQuery(foodCategory)} className='btn'>
+              {foodCategory}
+            </button>
+          ))}
+        </div>
+        <div className='plr-container flex'>
+          {loader ? (
+            <CardSkeleton amount={3} />
+          ) : (
+            data
+              .filter((item) => item.category === `${query}`)
+              .map((filterdItem) => (
+                <div className='plr-item-cnt'>
+                  <div className='plr-price flex-center'>
+                    <p>${filterdItem.price}</p>
+                  </div>
+                  <span className='plr-img flex-center'>
+                    <img src={filterdItem.img} width={150} />
+                  </span>
+                  <p></p>
+                  <div className='plr-rating'>
+                    <FontAwesomeIcon icon={faStar} />
+                    <FontAwesomeIcon icon={faStar} />
+                    <FontAwesomeIcon icon={faStar} />
+                    <FontAwesomeIcon icon={faStar} />
+                    <FontAwesomeIcon icon={faStar} />
+                    <span>{`(${filterdItem.rating})`}</span>
+                  </div>
+                  <p className='plr-name'>{filterdItem.name}</p>
+                  <p className='plr-desc'>{filterdItem.description}</p>
+                </div>
+              ))
+          )}
+        </div>
+        <div>
+          <Link to='/menu'>
+          <button className='btn explore-btn'>EXPLORE MENU</button>
+          </Link>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Popular
