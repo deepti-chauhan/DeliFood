f2305ab25055da42029431d9f5880824dd27d47a
Deepti <deepti22599@gmail.com>
2023-12-01 17:02:25 +0530

    fix : cart button item count
    nav cartbutton  item count remain same after page refresh

--- frontend/src/components/cart/CartButton.js
+++ frontend/src/components/cart/CartButton.js
@@ -1,11 +1,19 @@
 import { Link } from 'react-router-dom'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faCartShopping } from '@fortawesome/free-solid-svg-icons'
-import { useSelector } from 'react-redux'
+import { useDispatch, useSelector } from 'react-redux'
+import { useEffect } from 'react'
+import { fetchCart } from '../../cartStore/cartActions/cartSlice'
 
 const CartButton = () => {
   const items = useSelector((state) => state.cart.items)
+  const dispatch = useDispatch()
   const cartItemCount = items.reduce((acc, item) => acc + item.quantity, 0)
+
+
+  useEffect(()=> {
+    dispatch(fetchCart())
+  }, [])
   return (
     <Link to='/cart'>
       <button className='flex-center cart-item-icon'>
